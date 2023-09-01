c9f71041a4f2d84ae805f3d8be160a1530ac8711
Muhammad juman <juman@bitspro.com>
2023-09-01 18:30:45 +0500

    now fixed some of the bugs and now the typescript version

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
