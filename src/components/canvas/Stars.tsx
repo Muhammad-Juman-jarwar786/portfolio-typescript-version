ab9b88c74ead8650357756cd90e65621785de01b
Muhammad juman <juman@bitspro.com>
2023-09-01 18:55:40 +0500

    nothing to commit

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -1,3 +1,34 @@
+// import { useRef } from 'react';
+// import { Canvas, useFrame } from '@react-three/fiber';
+// import { Stars } from '@react-three/drei';
+
+// const MovingStars = () => {
+//   const ref = useRef<any>();
+
+//   useFrame((delta: any) => {
+//     if (ref.current) {
+//       ref.current.rotation.x += 0.01 * delta; // Increase the rotation speed
+//       ref.current.rotation.y += 0.02 * delta; // Increase the rotation speed
+//     }
+//   });
+
+//   return (
+//     <Canvas>
+//       <Stars ref={ref} />
+//     </Canvas>
+//   );
+// };
+
+// const StarsCanvas = () => {
+//   return (
+//     <div className="w-full h-auto absolute inset-0 z-[-1]">
+//       <MovingStars />
+//     </div>
+//   );
+// };
+
+// export default StarsCanvas;
+
 import { useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Stars } from '@react-three/drei';
@@ -29,3 +60,51 @@ const StarsCanvas = () => {
 };
 
 export default StarsCanvas;
+
+// import { useState, useRef, Suspense } from 'react';
+// import { Canvas, useFrame } from '@react-three/fiber';
+// import { Points, PointMaterial, Preload } from '@react-three/drei';
+// // import * as random from 'maath/random/dist/maath-random.esm';
+// // import * as random from 'maath/random/dist/maath-random.cjs';
+
+// const Stars = (props: any) => {
+//   const ref: React.MutableRefObject<any> = useRef();
+//   const [sphere] = useState(() =>
+//     random.inSphere(new Float32Array(5000), { radius: 1.2 })
+//   );
+
+//   useFrame((delta: any) => {
+//     ref.current.rotation.x -= delta / 10;
+//     ref.current.rotation.y -= delta / 15;
+//   });
+
+//   return (
+//     <group rotation={[0, 0, Math.PI / 4]}>
+//       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+//         <PointMaterial
+//           transparent
+//           color="#f272c8"
+//           size={0.002}
+//           sizeAttenuation={true}
+//           depthWrite={false}
+//         />
+//       </Points>
+//     </group>
+//   );
+// };
+
+// const StarsCanvas = () => {
+//   return (
+//     <div className="w-full h-auto absolute inset-0 z-[-1]">
+//       <Canvas camera={{ position: [0, 0, 1] }}>
+//         <Suspense fallback={null}>
+//           <Stars />
+//         </Suspense>
+
+//         <Preload all />
+//       </Canvas>
+//     </div>
+//   );
+// };
+
+// export default StarsCanvas;
